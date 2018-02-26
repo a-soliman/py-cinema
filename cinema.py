5ae2ddb5e18b0ecdc840e05ead19d20c68986ebf
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-02-26 09:13:58 -0800

    added a dictionary of films

---
+++ cinema.py
@@ -0,0 +1,18 @@
+films = {
+	"Finding Dory": {
+		"age": 3,
+		"avilable_seats": 5
+	},
+	"Bourne": {
+		"age": 18,
+		"avilable_seats": 5
+	},
+	"Tarazan": {
+		"age": 15,
+		"avilable_seats": 5
+	},
+	"Ghost Busters": {
+		"age": 12,
+		"avilable_seats": 5
+	}
+}
